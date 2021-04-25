2b2c12525639e93b4222a7d474be7d78a7277780
Pedro Pereira <75768229+WatchYoJet@users.noreply.github.com>
2021-04-25 23:11:39 +0100

    Add new system of displaying tests results (#1)

--- src/static/mooshak-da-feira.js
+++ src/static/mooshak-da-feira.js
@@ -1,11 +1,38 @@
 const submit = document.getElementById('submit');
 let globalTests = [];
-
+var testCounter = 0;
+var testEvaluated = 1;
 const socket = io();
+
+const resultColors = {
+  NOT_RAN: '#758173',
+  GRADER_EXCEPTION: '#f00',
+  COMPILE_ERROR: '#721121',
+  TIME_LIMIT_EXCEEDED: '#A9640F',
+  RUNTIME_ERROR: '#981F37',
+  WRONG_ANSWER: '#A01821',
+  SUCCESS: '#48653E',
+};
 
 socket.on('connect', () => {
   socket.emit('tests');
 });
+
+const reset = () => {
+  globalTests.forEach((_, i) => {
+    const header = document.getElementById(`test-header-${i}`);
+    header.getElementsByClassName('test-result')[0].innerHTML = formatResult('NOT_RAN');
+    document.getElementById(`test-button-${i}`).style.borderColor = resultColors['NOT_RAN'];
+    testEvaluated = 1;
+  });
+};
+
+const toggleButton = (test) => {
+  const el = document.getElementById(`test-button-${test}`);
+  el.classList.toggle('active');
+  const panel = el.nextElementSibling;
+  panel.style.maxHeight = panel.scrollHeight + 'px';
+};
 
 submit.onclick = async () => {
   const content = document.getElementById('code').value;
@@ -15,12 +42,13 @@ submit.onclick = async () => {
 
   const lastrun = document.getElementById('last-run');
   lastrun.innerText = new Date().toLocaleString();
+  reset();
 };
 
 socket.on('result', ({ test, ...data }) => {
   const testNode = document.getElementById(`test-${test}`);
-
-  testNode.getElementsByClassName('test-result')[0].innerHTML = formatResult(
+  const header = document.getElementById(`test-header-${test}`);
+  header.getElementsByClassName('test-result')[0].innerHTML = formatResult(
     data.status || 'UNKNOWN'
   );
   testNode.getElementsByClassName('test-stdout')[0].innerText = data.stdout;
@@ -29,6 +57,14 @@ socket.on('result', ({ test, ...data }) => {
   testNode.getElementsByClassName('test-signal')[0].innerText = `Exit signal: ${
     data.signal || 'none'
   }`;
+
+  document.getElementById(`test-button-${test}`).style.borderColor =
+    resultColors[data.status] || '#000';
+  if (data.status != 'SUCCESS') toggleButton(test);
+  const progress = document.getElementById('progressBar');
+  progress.innerHTML = `${testEvaluated} / ${testCounter}`;
+  if (testEvaluated == testCounter) progress.innerHTML += ' All Done!';
+  testEvaluated += 1;
 });
 
 socket.on('done', () => {
@@ -38,17 +74,25 @@ socket.on('done', () => {
 const formatTags = (tags) => tags.map((tag) => `<span class="test-tag">${tag}</span>`);
 
 const formatResult = (result) => {
-  const color =
-    {
-      NOT_RAN: '#758173',
-      GRADER_EXCEPTION: '#f00',
-      COMPILE_ERROR: '#721121',
-      TIME_LIMIT_EXCEEDED: '#A9640F',
-      RUNTIME_ERROR: '#981F37',
-      WRONG_ANSWER: '#A01821',
-      SUCCESS: '#48653E',
-    }[result] || '#000';
+  const color = resultColors[result] || '#000';
   return `<span class="test-tag" style="background: ${color};">${result.replace('_', ' ')}</span>`;
+};
+
+const injectAccordionListeners = () => {
+  /* used for results buttons animations*/
+  const acc = document.getElementsByClassName('accordion');
+
+  [...acc].forEach((el) =>
+    el.addEventListener('click', () => {
+      el.classList.toggle('active');
+      const panel = el.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    })
+  );
 };
 
 socket.on('tests', (tests) => {
@@ -59,23 +103,41 @@ socket.on('tests', (tests) => {
   testsDiv.innerHTML = '';
 
   tests.forEach(({ tags, description }, i) => {
-    testsDiv.innerHTML += `
-    <div class="test" id="test-${i}">
-      <div class="test-header">
+    testsDiv.innerHTML += `<button class="accordion" id="test-button-${i}" style="border-color: ${
+      resultColors['NOT_RAN']
+    };">
+      <div class="test-header" id="test-header-${i}">
         <h4>Test ${i + 1}</h4>
         <p>${formatTags(tags)}</p>
         <p class="test-result">${formatResult('NOT_RAN')}</p>
-      </div>
-      <div class="test-content">
-        <p>${description}</p>
-        <p class="test-signal"></p>
-        <p class="test-code"></p>
-        <div class="test-output">
-          <pre class="test-stdout"></pre>
-          <pre class="test-stderr"></pre>
+      </div></button>
+	    <div class="panel">
+        <div class="test" id="test-${i}">
+          <div class="test-content">
+            <p>${description}</p>
+            <p class="test-signal"></p>
+            <p class="test-code"></p>
+            <div class="test-output">
+              <pre class="test-stdout"></pre>
+              <pre class="test-stderr"></pre>
+            </div>
+          </div>
+          <div class="block">
+            <p>Input</p>
+            <textarea id="code" rows="30" cols="60"></textarea>
+          </div>
+          <div class="block">
+            <p>Expected Output</p>
+            <textarea id="code" rows="30" cols="60"></textarea>
+          </div>
+          <div class="block">
+            <p>Program Output</p>
+            <textarea id="code" rows="30" cols="60"></textarea>
+          </div>
         </div>
-      </div>
-    </div>
-  `;
+	    </div>
+      `;
+    injectAccordionListeners();
+    testCounter += 1;
   });
 });
